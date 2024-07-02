2e9b4bf2a3682ff56cd252e389fa79898eb24ac0
Mirza Saikat Ahmmed <mirzasaikatahmmed@gmail.com>
2024-07-03 00:19:26 +0600

    Update

--- Practice/MySQL/Registration.php
+++ Practice/MySQL/Registration.php
@@ -21,23 +21,29 @@
                         <input type="text" id="fname" name="fname" placeholder="Your First Name" value="<?php echo isset($_SESSION['fname']) ? $_SESSION['fname'] : ""; ?>">
                         <?php echo isset($_SESSION['fnameErr']) ? $_SESSION['fnameErr'] : ""; ?>
                         <br><br>
+
                         <label for="lname"><strong>Last Name:</strong></label>
                         <input type="text" id="lname" name="lname" placeholder="Your Last Name" value="<?php echo isset($_SESSION['lname']) ? $_SESSION['lname'] : ""; ?>">
                         <?php echo isset($_SESSION['lnameErr']) ? $_SESSION['lnameErr'] : ""; ?>
                         <br><br>
+
                         <label><strong>Gender:</strong></label>
                         <input type="radio" id="male" name="gender" value="Male" checked>
                         <label for="male">Male</label>
                         <input type="radio" id="female" name="gender" value="Female">
-                        <label for="female">Female</label><br><br>
+                        <label for="female">Female</label>
+                        <br><br>
+
                         <label for="faName"><strong>Father's Name:</strong></label>
                         <input type="text" id="faName" name="faName" placeholder="Your Father's Name" value="<?php echo isset($_SESSION['faName']) ? $_SESSION['faName'] : ""; ?>">
                         <?php echo isset($_SESSION['faNameErr']) ? $_SESSION['faNameErr'] : ""; ?>
                         <br><br>
+
                         <label for="moName"><strong>Mother's Name:</strong></label>
                         <input type="text" id="moName" name="moName" placeholder="Your Mother's Name" value="<?php echo isset($_SESSION['moName']) ? $_SESSION['moName'] : ""; ?>">
                         <?php echo isset($_SESSION['moNameErr']) ? $_SESSION['moNameErr'] : ""; ?>
                         <br><br>
+
                         <label for="bGroup"><strong>Blood Group:</strong></label>
                         <select id="bGroup" name="bGroup">
                             <option value="A+">A+</option>
@@ -48,7 +54,9 @@
                             <option value="B-">B-</option>
                             <option value="AB-">AB-</option>
                             <option value="O-">O-</option>
-                        </select><br><br>
+                        </select>
+                        <br><br>
+
                         <label for="religion"><strong>Religion:</strong></label>
                         <select id="religion" name="religion">
                             <option value="Islam">Islam</option>
@@ -63,14 +71,17 @@
                         <input type="text" id="email" name="email" placeholder="Your Email Address" value="<?php echo isset($_SESSION['email']) ? $_SESSION['email'] : ""; ?>">
                         <?php echo isset($_SESSION['emailErr']) ? $_SESSION['emailErr'] : ""; ?>
                         <br><br>
+
                         <label for="mNumber"><strong>Phone/Mobile:</strong></label>
                         <input type="number" id="mNumber" name="mNumber" placeholder="Your Contact Number" value="<?php echo isset($_SESSION['mNumber']) ? $_SESSION['mNumber'] : ""; ?>">
                         <?php echo isset($_SESSION['mNumberErr']) ? $_SESSION['mNumberErr'] : ""; ?>
                         <br><br>
+
                         <label for="website"><strong>Website:</strong></label>
                         <input type="url" id="website" name="website" placeholder="Your Website URL" value="<?php echo isset($_SESSION['website']) ? $_SESSION['website'] : ""; ?>">
                         <?php echo isset($_SESSION['websiteErr']) ? $_SESSION['websiteErr'] : ""; ?>
                         <br><br>
+
                         <label>Address: </label>
                         <fieldset>
                             <legend>Present Address</legend>
@@ -80,14 +91,18 @@
                                 <option value="India">India</option>
                                 <option value="Pakistan">Pakistan</option>
                                 <option value="Sri Lanka">Sri Lanka</option>
-                            </select><br><br>
+                            </select>
+                            <br><br>
+
                             <label for="city"><strong>City:</strong></label>
                             <select id="city" name="city">
                                 <option value="Dhaka">Dhaka</option>
                                 <option value="Chittagong">Chittagong</option>
                                 <option value="Rajshahi">Rajshahi</option>
                                 <option value="Khulna">Khulna</option>
-                            </select><br><br>
+                            </select>
+                            <br><br>
+
                             <label for="pAddress"><strong>Present Address:</strong></label><br>
                             <textarea id="pAddress" name="pAddress" rows="4" cols="50"><?php echo isset($_SESSION['pAddress']) ? $_SESSION['pAddress'] : ""; ?></textarea><br><br>
                             <label for="zip"><strong>ZIP Code:</strong></label>
@@ -103,14 +118,17 @@
                         <input type="text" id="username" name="username" placeholder="Username" value="<?php echo isset($_SESSION['username']) ? $_SESSION['username'] : ""; ?>">
                         <?php echo isset($_SESSION['usernameErr']) ? $_SESSION['usernameErr'] : ""; ?>
                         <br><br>
+
                         <label for="password"><strong>Password:</strong></label>
                         <input type="password" id="password" name="password" placeholder="Password" value="<?php echo isset($_SESSION['password']) ? $_SESSION['password'] : ""; ?>">
                         <?php echo isset($_SESSION['passwordErr']) ? $_SESSION['passwordErr'] : ""; ?>
                         <br><br>
+
                         <label for="cPassword"><strong>Confirm Password:</strong></label>
                         <input type="password" id="cPassword" name="cPassword" placeholder="Confirm Password" value="<?php echo isset($_SESSION['cPassword']) ? $_SESSION['cPassword'] : ""; ?>">
                         <?php echo isset($_SESSION['cPasswordErr']) ? $_SESSION['cPasswordErr'] : ""; ?>
                         <br><br>
+                        
                         <input type="submit" value="Register">
                     </fieldset>
                 </td>
